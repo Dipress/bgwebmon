bc69e86763f33d2d8f0201c3a13ee229cc7e9f6f
Roman Budnikov <d3.supro@gmail.com>
2012-10-11 11:21:26 +0300

    nodestatisctic

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -5,6 +5,9 @@ before_filter :ip_check
   def index
     @title = "Список точек подключения"
     @nodes = ContractParameterType7Value.where(:pid => 54).order('title ASC')
+    @regions = regions_array(@nodes)
+    @nodes = []
+    @regions.each{|r| @nodes << nodes_from_region(r)}
   end
 
   def show
@@ -33,4 +36,19 @@ private
                     :comment => contract.comment, 
                     :tariffs => Contract.tariffs_array(cid)}
 	end
+
+  def regions_array(nodes)
+    scan = /^([А-Я]|[а-я])+\ \-\ /
+    array = []
+    nodes.each{|z| array << z.title.match(scan).to_s.gsub(/\ \-\ /,"")}
+    return array.uniq
+  end
+
+  def nodes_from_region(region)
+    array = []
+    ContractParameterType7Value.where("pid='54' and title like '#{region}%'").order('title ASC').each{|n|
+      array << {:id => n.id, :title => n.title}}
+    return array = {:region => region, :nodes => array}
+  end
+
 end
