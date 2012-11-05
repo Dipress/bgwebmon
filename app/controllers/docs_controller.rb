f22ab51ffc46c8805d6fb366aee9f6c87655533a
Roman Budnikov <d3.supro@gmail.com>
2012-11-05 13:01:49 +0200

    changes

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -4,12 +4,8 @@ class DocsController < ApplicationController
   before_filter :ip_check
   before_filter :sadmin
   def index
-  	page = params[:page]
-    params[:page].nil? ? page = 1 : page = params[:page] 
-    @contracts = Contract.paginate(:page => page, 
-                             	   :per_page => 20,
-                             	   :order => "title ASC").where(["pgid=? or pgid=? and status=?",1,2,0])
-    @title= "Данные по договорам"
+    @contracts = Contract.where(["pgid=? or pgid=? and status=?",1,2,0]).order("title ASC")
+    @title= "Статусы наличия подписаных договоров"
   	@user = current_user()
   end
 end
