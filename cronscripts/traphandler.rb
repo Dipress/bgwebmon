199b442ac6e4a5ed2281992c0319db7f06245e57
Roman Budnikov <d3.supro@gmail.com>
2012-11-14 15:11:07 +0200

    tasks added

--- cronscripts/traphandler.rb
+++ cronscripts/traphandler.rb
@@ -16,19 +16,21 @@ rxtx=`#{cmd}`.gsub(/[0-9].+\:.+/).first.gsub(/[0-9]+\:/,"")
 
 rxtx.scan(/[0-9]+?\,[0-9]+e\+[0-9][0-9]/).each{|s| array << s}
 
-if array[5].nil?
+x = rxtx.split("\ ")
+
+if x[2].to_f.eql? 0.0
   upload=0
   rx=array[1].to_f
 else
-  lrx=array[5].gsub(/\,/,"\.").to_f
+  lrx=x[2].gsub(/\ /,"").to_f
   rx=array[1].to_f
   (rx<lrx) ? upload=0 : upload=(((rx-lrx)*8)/60)*0.605
 end
-if array[6].nil?
+if x[3].to_f.eql? 0.0
   download=0
   tx=array[2].to_f
 else
-  ltx=array[6].gsub(/\,/,"\.").gsub(/\ /,"").to_f
+  ltx=x[3].gsub(/\ /,"").to_f
   tx=array[2].to_f
   (tx<ltx) ? download=0 : download=(((tx-ltx)*8)/60)*0.605
 end
