1a1e4d1c7d7f7a605567fae44718f25774206d52
Roman Budnikov <d3.supro@gmail.com>
2012-11-27 14:49:26 +0200

    mon css change

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,11 +9,11 @@ class Task < ActiveRecord::Base
   has_many :tfiles
 
   validates :title, :presence => {:message => "Заголовок обязательное поле"},
-            :length =>{:within => 1..120,
-                       :message => 'Заголовок - поле должно содержать от 1 до 120 символов'}
+                   :length =>{:within => 1..120,
+                              :message => 'Заголовок - поле должно содержать от 1 до 120 символов'}
   validates :text, :presence => {:message => "Текст - обязательное поле"},
-            :length =>{:within => 1..1000,
-                       :message => 'Текст - поле должно содержать от 1 до 1000 символов'}
+                   :length =>{:within => 1..1000,
+                              :message => 'Текст - поле должно содержать от 1 до 1000 символов'}
 
   validates :observers, :presence => {:message => "Наблюдатели не выбраны"}
 end
