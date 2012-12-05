9b7fa32be1f5cfb42bf47ac4e719d3c8fcb82332
Roman Budnikov <d3.supro@gmail.com>
2012-12-05 11:51:28 +0200

    bootstrap changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,7 @@ module ApplicationHelper
 		def_title = "Bgwebmon"
 		@title.nil? ? def_title : "#{def_title} | #{@title}"
 	end
+	def li_menu(controllername, content)
+		content_tag :li, content, class: (controller.controller_name.eql?(controllername) ? "active" : nil)
+	end
 end
