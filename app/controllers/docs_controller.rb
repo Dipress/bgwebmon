0a0a53c61921453f7ad1c37f23e5bf035c751730
Roman Budnikov <d3.supro@gmail.com>
2013-02-26 10:38:49 +0200

    docs status change

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -4,14 +4,14 @@ class DocsController < ApplicationController
   before_filter :ip_check
   before_filter :sadmin
   def index
-    @contracts = Contract.where( [ "(pgid=? or pgid=?) and status=?", 1, 2, 0 ] ).order "title ASC"
+    @contracts = Contract.where( [ "(pgid=? or pgid=?) and (status=? or status=?)", 1, 2, 0, 2 ] ).order "title ASC"
     @most = Contract.where( [ "title LIKE ?", "%-000" ] ).order "title ASC"
     @title= "Статусы наличия подписаных договоров"
   	@user = current_user
   end
 
   def show
-  	@contracts = Contract.where( [ "status=? AND title LIKE ? AND (pgid=? OR pgid=?)", 0, params[ :title ]+"%", 1, 2 ] ).order "title ASC"
+  	@contracts = Contract.where( [ "(status=? or status=?) AND title LIKE ? AND (pgid=? OR pgid=?)", 0, 2, params[ :title ]+"%", 1, 2 ] ).order "title ASC"
     @most = Contract.where( [ "title LIKE ?", "%-000" ] ).order "title ASC"
     @title= "Статусы наличия подписаных договоров"
   	@user = current_user
