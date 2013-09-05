845d067db2bd4053f03179f637e2b8d237376fbc
Roman Budnikov <budnikov1990@gmail.com>
2013-09-06 00:21:06 +0300

    fixes

--- app/tasks/changes_tasks.rb
+++ app/tasks/changes_tasks.rb
@@ -6,17 +6,17 @@ class ChangesTasks
     [{contract: 199, email: "ilnicski.vi@gmail.com"}, 
      {contract: 441, email: "djzubik@gmail.com"}, 
      {contract: 253, email: "bah@crimeainfo.com"}].each do |u|
-      statuses = Status.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND contract_status_log.date >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: ["Активен", "В отключении", "Отключен", "Приостановлен", "В подключении"][c.status], comment: c.comment, updated_at: I18n.localize(c.date)} }
-      limits = Limit.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND log_contract_limit.dt >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.nvalue, comment: c.comment, updated_at: I18n.localize(c.dt)} }
-      payments = Payment.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND contract_payment.lm >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.summa, comment: c.comment, updated_at: I18n.localize(c.lm)} }
-      if statuses.length > 0 && limits.length > 0 && payments.length > 0
+      statuses = Status.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND contract_status_log.date >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: ["Активен", "В отключении", "Отключен", "Приостановлен", "В подключении"][c.status], comment: c.comment, updated_at: I18n.localize(c.date-3.hours)} }
+      limits = Limit.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND log_contract_limit.dt >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.nvalue, comment: c.comment, updated_at: I18n.localize(c.dt-3.hours)} }
+      payments = Payment.joins(contract: :contract_parameter_type8).where("contract_parameter_type_8.val=#{u[:contract]} AND contract_payment.lm >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.summa, comment: c.comment, updated_at: I18n.localize(c.lm-3.hours)} }
+      if statuses.length > 0 || limits.length > 0 || payments.length > 0
         Changemailer.send_changes(limits, payments, statuses, u[:email], hour_ago).deliver
       end
     end
-    statuses = Status.where("contract_status_log.date >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: ["Активен", "В отключении", "Отключен", "Приостановлен", "В подключении"][c.status], comment: c.comment, updated_at: I18n.localize(c.date)} }
-    limits = Limit.where("log_contract_limit.dt >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.nvalue, comment: c.comment, updated_at: I18n.localize(c.dt)} }
-    payments = Payment.where("contract_payment.lm >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.summa, comment: c.comment, updated_at: I18n.localize(c.lm)} }
-    if statuses.length > 0 && limits.length > 0 && payments.length > 0
+    statuses = Status.where("contract_status_log.date >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: ["Активен", "В отключении", "Отключен", "Приостановлен", "В подключении"][c.status], comment: c.comment, updated_at: I18n.localize(c.date-3.hours)} }
+    limits = Limit.where("log_contract_limit.dt >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.nvalue, comment: c.comment, updated_at: I18n.localize(c.dt-3.hours)} }
+    payments = Payment.where("contract_payment.lm >= '#{hour_ago}'").map{|c| {changer: ((!c.user.nil?)? c.user.name : "Сервер") , changed: "#{c.contract.title} - #{c.contract.comment}", change: c.summa, comment: c.comment, updated_at: I18n.localize(c.lm-3.hours)} }
+    if statuses.length > 0 || limits.length > 0 || payments.length > 0
       Changemailer.send_changes(limits, payments, statuses, "statuses@crimeainfo.com", hour_ago).deliver
     end
     nil
