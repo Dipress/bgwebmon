dcc7226b8aac5cf1ddc8bc833cebb8f4f889006d
Dipress <budnikovdo@gmail.com>
2013-10-30 10:59:05 +0200

    enable agentpayment email

--- app/observers/agent_payment_observer.rb
+++ app/observers/agent_payment_observer.rb
@@ -3,11 +3,11 @@ class AgentPaymentObserver < ActiveRecord::Observer
 
   def after_create record
     Paymentmailer.delay.payment_added record, "pays@crimeainfo.com"
-    PaymentTasks.new.delay.charge_added record
+    #PaymentTasks.new.delay.charge_added record
   end
 
   def after_update record
     Paymentmailer.delay.payment_processed record, "pays@crimeainfo.com"
-    PaymentTasks.new.delay.payment_processed record
+    #PaymentTasks.new.delay.payment_processed record
   end
 end
