06ad4bb8dc85bcff35f6a3680d845c9823dc99a4
Dipress <budnikovdo@gmail.com>
2013-11-14 14:38:30 +0200

    fix agent_payment observer

--- app/observers/agent_payment_observer.rb
+++ app/observers/agent_payment_observer.rb
@@ -2,12 +2,14 @@ class AgentPaymentObserver < ActiveRecord::Observer
   observe :agent_payment
 
   def after_create record
-    Paymentmailer.delay.payment_added record, "pays@crimeainfo.com"
+    #Paymentmailer.delay.payment_added record, "pays@crimeainfo.com"
     #PaymentTasks.new.delay.charge_added record
+    Paymentmailer.payment_added(record, "pays@crimeainfo.com").deliver
   end
 
   def after_update record
-    Paymentmailer.delay.payment_processed record, "pays@crimeainfo.com"
+    #Paymentmailer.delay.payment_processed record, "pays@crimeainfo.com"
     #PaymentTasks.new.delay.payment_processed record
+    Paymentmailer.payment_added(record, "pays@crimeainfo.com").deliver
   end
 end
