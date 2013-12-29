7192ab1435cccf6aade1d981d112e1ef708739b2
Roman Budnikov <budnikov1990@gmail.com>
2013-12-29 19:26:33 +0200

    beta monitoring fix 2

--- app/serializers/dialupip_serializer.rb
+++ app/serializers/dialupip_serializer.rb
@@ -8,12 +8,12 @@ class DialupipSerializer < ActiveModel::Serializer
     if rxtx != "" && !rxtx.nil?
       rxtx=rxtx.gsub(/[0-9].+\:.+/).first.gsub(/[0-9]+\:\ /, "")
       if rxtx =~ /-nan/
-        return 'danger'
+        return 'error'
       else
         return 'success'
       end
     else
-      return 'danger'
+      return 'error'
     end
   end
 end
