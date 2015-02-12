e6bedfc6281d2dd2e1d2d6b60de5ad7807f84634
Dipress <budnikovdo@gmail.com>
2015-02-12 15:48:10 +0300

    add to mikrotik inet members

--- app/tasks/mikrotik_members_tasks.rb
+++ app/tasks/mikrotik_members_tasks.rb
@@ -9,7 +9,11 @@ class MikrotikMembersTasks
 		members = InetService.where(deviceId: 12)
 
 		#Соединяемся с MikroTik.
-		mikrotik = MTik::Connection.new(host: "172.28.200.42", pass: "temp_mikrotik")
+
+		host = ENV["MIKROTIK_HOST"]
+		password = ENV["MIKROTIK_PASSWORD"]
+
+		mikrotik = MTik::Connection.new(host: host, pass: password)
 
 		#Создаем пустой массив, в котором будет хранить данные о текущих абонентах
 		#которые сейчас присутствуют в базе Mikrotik. Нам это будет нужно для того,
@@ -32,68 +36,49 @@ class MikrotikMembersTasks
     end
 
     #Начинаем процесс добавление новых, актуальных данных о пользователях
-    members.map do |m|
+
+    add_members = members.map do |m|
     	if m.deviceState.to_i.eql?(1)
     		if m.deviceOptions.to_i.eql?(2)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_64kb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(4)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_128kb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(5)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_256kb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(6)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_512kb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(8)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_1Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(11)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_1.5Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(9)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_2Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(12)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_2.5Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(10)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_3Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(13)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_4Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(14)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_5Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(15)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_6Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(16)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_7Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(17)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_8Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(18)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_9Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(20)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_10Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(26)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_12Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(28)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_15Mb"})
-    			sleep(0.3)
     		elsif m.deviceOptions.to_i.eql?(25)
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_20Mb"})
-    			sleep(0.3)
     		else
     			mikrotik.get_reply("/ppp/secret/add", {"local-address" => "10.0.0.0"}, {"name" =>"#{m.login}"}, {"password" => "#{m.password}"}, {"remote-address" => "#{m.addressFrom.to_s.bytes.to_a.join('.')}"}, {"profile" => "vpn_2Mb"})
-    			sleep(0.3)
     		end
     	end
     end
